7af002c9988d5d7f0cec7690aa7dd4cd7c87a834
Kac1per <kac1per@gmail.com>
2021-10-19 02:52:56 +0200

    drobna poprawka w metadanych

--- source/__source/layouts/master.blade.php
+++ source/__source/layouts/master.blade.php
@@ -9,7 +9,7 @@
         <meta property="og:site_name" content="{{ $page->nazwaWitryny }}"/>
         <meta property="og:title" content="{{ $page->title() ?  $page->title() . ' | ' : '' }}{{ $page->nazwaWitryny }}"/>
         <meta property="og:description" content="{{ $page->description ?? $page->opisWitryny }}"/>
-        <meta property="og:url" content="{{ $page->getPath() }}"/>
+        <meta property="og:url" content="{{ $page->baseUrl }}{{ $page->getPath() == '/home' ? '' : $page->getPath() }}"/>
         <meta property="og:image" content="{{ $page->baseUrl }}/assets/img/social-share.png"/>
         <meta property="og:type" content="website"/>
 
