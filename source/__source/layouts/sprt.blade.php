4019c7d230fca8f41f3c9a2878c2c82519c1a98d
Kac1per <kac1per@gmail.com>
2022-03-13 23:03:38 +0100

    szybka poprawka do ostatniej zmiany

--- source/__source/layouts/sprt.blade.php
+++ source/__source/layouts/sprt.blade.php
@@ -2,11 +2,11 @@
 
 @section('body')
 <div class="container max-w-6xl mx-auto px-6 py-4">
-    <div class="text-pink-600 dark:text-indigo-300 text-xl hidden lg:block">
+    <div class="text-xl hidden lg:block">
         <nav>
-            <a href="/krok-po-kroku" class="border-b-0 font-heading font-bold tracking-wider">
+            <span class="border-b-0 font-heading font-bold tracking-wider">
                 Wsparcie
-            </a>
+            </span>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="inline-block transform -rotate-90 -scale-y-100 mt-1 h-5">
                 <path fill-rule="evenodd" d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
             </svg>
